aab57ec68fa87de9b25c8c0ffaa355406daa5858
Ryan Lail <me@ryanlail.com>
2021-11-22 18:15:42 +0000

    hyperparameter tuning

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -220,7 +220,11 @@ class Regressor():
             x, _ = self._preprocessor(x, training = False)
         tensor_x = torch.Tensor(x).to(device)
 
-        return self.model(tensor_x).detach().cpu().numpy()
+        prediction = self.model.eval()(tensor_x).detach().cpu().numpy()
+        # Put model back into train mode after a prediction has been made
+        self.model.train()
+
+        return prediction
 
 
         #######################################################################
@@ -282,7 +286,7 @@ def load_regressor():
 
 
 
-def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val,x_test,y_test):
+def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val):
 
  
     # Ensure to add whatever inputs you deem necessary to this function
@@ -301,11 +305,6 @@ def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val,x_test,y_test):
     #######################################################################
     #                       ** START OF YOUR CODE **
     #######################################################################
-
-    # learning rate (0.000001 - 10 (*10))
-    # neurons (5, 30 (+3))
-    # hidden layers (1, 5 (+1))
-    # early stop n
     
     # maunual exprimentation:
         # mini_batch size
@@ -313,25 +312,28 @@ def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val,x_test,y_test):
         # dropout rate
 
 
-    lr_array=np.array([0.01,0.1,1,10])
-    neurons_array=np.array([5,10,20,30])
-    hidden_layers_array=np.array([1,2,3,4])
-    n_array=np.array([2,5,10])
+    lr_array = np.array([0.01,0.1,1,10])
+    neurons_array = np.array([5,10,20,30])
+    hidden_layers_array = np.array([1,2,3,4])
+    n_array = np.array([2,5,10])
 
-    results=np.zeros((len(hidden_layers_array),len(neurons_array),len(lr_array),len(n_array)))
+    results = np.zeros((len(hidden_layers_array), len(neurons_array), len(lr_array), len(n_array)))
 
+    # TODO remove tqdm
     for i in tqdm(range(len(hidden_layers_array))):
-        for j in tqdm(range(len(neurons_array))):
-            for k in tqdm(range(len(lr_array))):
-                for l in tqdm(range(len(n_array))):
+        for j in range(len(neurons_array)):
+            for k in range(len(lr_array)):
+                for l in range(len(n_array)):
 
-                    regressor = Regressor(x_train, hidden_layers=hidden_layers_array[i],neurons=neurons_array[j],learning_rate=lr_array[k], nb_epoch = 100,dropout=0.25)
-                    regressor.fit(x_train, y_train,x_val=x_val,y_val=y_val,mini_batch_size=50,early_stop_n=n_array[l])
+                    regressor = Regressor(x_train, hidden_layers=hidden_layers_array[i], neurons=neurons_array[j], learning_rate=lr_array[k], nb_epoch = 100, dropout=0.5)
+                    regressor.fit(x_train, y_train, x_val=x_val, y_val=y_val, mini_batch_size=50, early_stop_n=n_array[l])
 
                     
-                    results[i,j,k,l] = regressor.score(x_test, y_test)
+                    results[i,j,k,l] = regressor.score(x_val, y_val)
 
-    best_ids=np.where(results==np.max(results))
+    # TODO remove save
+    np.save("hyp_tunin.npy", results)
+    best_ids=np.where(results==np.min(results))
 
     return hidden_layers_array[best_ids[0][0]],neurons_array[best_ids[1][0]],lr_array[best_ids[2][0]],n_array[best_ids[3][0]]
 
@@ -361,20 +363,20 @@ def example_main():
     x_test = test.loc[:, data.columns != output_label]
     y_test = test.loc[:, [output_label]]
 
-    regressor = Regressor(x_train, hidden_layers=2,neurons=27,learning_rate=0.1, nb_epoch = 100,dropout=0.25)
-    regressor.fit(x_train, y_train,x_val=x_val,y_val=y_val,mini_batch_size=50,early_stop_n=5)
+    regressor = Regressor(x_train, hidden_layers=1, neurons=30, learning_rate=0.1, nb_epoch=100, dropout=0.5)
+    regressor.fit(x_train, y_train, x_val=x_val, y_val=y_val, mini_batch_size=50, early_stop_n=5)
 
     # Error
     error = regressor.score(x_test, y_test)
     print("\nRegressor error: {}\n".format(error))
 
     print("\nPerforming Hyper Parameter Tuning")
-    best_hidden_layers,best_neurons,best_learning_rate,best_early_stop_n=RegressorHyperParameterSearch(x_train,y_train,x_val=x_val,y_val=y_val,x_test=x_test,y_test=y_test)
+    best_hidden_layers, best_neurons, best_learning_rate, best_early_stop_n = RegressorHyperParameterSearch(x_train,y_train,x_val,y_val)
     print('\nBest Params:')
     print('Hidden Layers: ',best_hidden_layers)
     print('Neurons: ',best_neurons)
     print('Learning Rate', best_learning_rate)
-    print('Best Early Stop n', best_learning_rate)
+    print('Best Early Stop n', best_early_stop_n)
 
     print("\nTraining With Best Params")
     best_regressor=Regressor(x_train,hidden_layers=best_hidden_layers,neurons=best_neurons,learning_rate=best_learning_rate,nb_epoch=1000,dropout=0.25)
@@ -386,6 +388,7 @@ def example_main():
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # TODO remove print
     print(device)
     example_main()
 
