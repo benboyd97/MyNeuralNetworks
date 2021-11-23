e78fe644519d00977a0283d21e0e2369cade5b06
Ben Boyd <bboyd1997@hotmai.co.uk>
2021-11-23 13:49:36 +0000

    back to old commit

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -15,26 +15,20 @@ from sklearn.metrics import mean_absolute_error
 class NeuralNetwork(nn.Module):
     def __init__(self, input_size, output_size, hidden_layers, neurons, dropout):
         super(NeuralNetwork, self).__init__()
+        
+        # input layer
+        layers = [nn.Linear(input_size, neurons), nn.ReLU()]
 
-        # Single linear layer with relu activation and dropout
-        linear_relu_block = nn.Sequential(
-            nn.Linear(neurons, neurons),
-            nn.ReLU(),
-            nn.Dropout(dropout)
-        )
-
-        # Stack linear layer blocks as hidden layers
-        linear_relu_stack = nn.Sequential()
+        # hidden layers
         for _ in range(hidden_layers):
-            linear_relu_stack = nn.Sequential(*(list(linear_relu_stack)+list(linear_relu_block)))
+            layers.append(nn.Linear(neurons, neurons))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
 
-        # Add hidden layers to input and output layers
-        self.main = nn.Sequential(
-            nn.Linear(input_size, neurons),
-            nn.ReLU(),
-            linear_relu_stack,
-            nn.Linear(neurons, output_size)
-        )
+        # output layer
+        layers.append(nn.Linear(neurons, output_size))
+
+        self.main = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.main(x)
@@ -42,7 +36,7 @@ class NeuralNetwork(nn.Module):
 
 class Regressor():
 
-    def __init__(self, x, nb_epoch=100, learning_rate=0.01, hidden_layers=5, neurons=30, dropout=0.25):
+    def __init__(self, x, nb_epoch=100, learning_rate=0.01, hidden_layers=5, neurons=30, dropout=0.5):
         # You can add any input parameters you need
         # Remember to set them with a default value for LabTS tests
         """ 
@@ -103,28 +97,16 @@ class Regressor():
         if training == True: 
             # one-hot encoding
             self.one_hot = preprocessing.LabelBinarizer().fit(x['ocean_proximity'])
-            hot_arr=self.one_hot.transform(x['ocean_proximity'])
-            x=x.drop(columns=['ocean_proximity'])
-            
-            for i in range(np.shape(hot_arr)[1]):
-
-                x['Ocean_Prox_'+str(i)]=hot_arr[:,i]
-           
+            x['ocean_proximity'] = np.argmax(self.one_hot.transform(x['ocean_proximity']), axis=1) 
 
             # save x columnwise normalisation constants
             self.norm_x_min = x.min() 
             self.norm_x_max = x.max() 
             self.norm_x_mean = x.mean() 
-            self.norm_x_std = x.std()
+            self.norm_x_std = x.std() 
         else:
             # perform one-hot tranform 
-            hot_arr=self.one_hot.transform(x['ocean_proximity'])
-            x=x.drop(columns=['ocean_proximity'])
-
-            for i in range(np.shape(hot_arr)[1]):
-
-                x['Ocean_Prox_'+str(i)]=hot_arr[:,i]
-
+            x['ocean_proximity'] = np.argmax(self.one_hot.transform(x['ocean_proximity']), axis=1)     
 
         if norm_method == 'standard':
             # calculate using saved normalisation constants from training
@@ -204,8 +186,6 @@ class Regressor():
                     self.model.load_state_dict(model_backup)
                     return self.model
 
-            # TODO regularization
-
         return self.model
 
         #######################################################################
@@ -235,7 +215,11 @@ class Regressor():
             x, _ = self._preprocessor(x, training = False)
         tensor_x = torch.Tensor(x)
 
-        return self.model(tensor_x).detach().numpy()
+        prediction = self.model.eval()(tensor_x).detach().numpy()
+        # Put model back into train mode after a prediction has been made
+        self.model.train()
+
+        return prediction
 
 
         #######################################################################
@@ -297,7 +281,7 @@ def load_regressor():
 
 
 
-def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val,x_test,y_test):
+def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val):
 
  
     # Ensure to add whatever inputs you deem necessary to this function
@@ -316,32 +300,33 @@ def RegressorHyperParameterSearch(x_train,y_train,x_val,y_val,x_test,y_test):
     #######################################################################
     #                       ** START OF YOUR CODE **
     #######################################################################
-
+    
     # maunual exprimentation:
         # mini_batch size
         # max_epochs
         # dropout rate
 
 
-    lr_array=np.array([0.0001,0.001,0.01,0.1,1,10])
-    neurons_array=np.array([5,10,15,20,25,30])
-    hidden_layers_array=np.array([1,2,3,4,5])
-    n_array=np.array([2,5,10])
+    lr_array = np.array([0.01,0.1,1,10])
+    neurons_array = np.array([5,10,20,30])
+    hidden_layers_array = np.array([1,2,3,4])
+    n_array = np.array([2,5,10])
 
-    results=np.zeros((len(hidden_layers_array),len(neurons_array),len(lr_array),len(n_array)))
+    results = np.zeros((len(hidden_layers_array), len(neurons_array), len(lr_array), len(n_array)))
 
     for i in range(len(hidden_layers_array)):
         for j in range(len(neurons_array)):
             for k in range(len(lr_array)):
                 for l in range(len(n_array)):
 
-                    regressor = Regressor(x_train, hidden_layers=hidden_layers_array[i],neurons=neurons_array[j],learning_rate=lr_array[k], nb_epoch = 1000,dropout=0.25)
-                    regressor.fit(x_train, y_train,x_val=x_val,y_val=y_val,mini_batch_size=100,early_stop_n=n_array[l])
+                    regressor = Regressor(x_train, hidden_layers=hidden_layers_array[i], neurons=neurons_array[j], learning_rate=lr_array[k], nb_epoch = 100, dropout=0.5)
+                    regressor.fit(x_train, y_train, x_val=x_val, y_val=y_val, mini_batch_size=50, early_stop_n=n_array[l])
 
                     
-                    results[i,j,k,l] = regressor.score(x_test, y_test)
+                    results[i,j,k,l] = regressor.score(x_val, y_val)
 
-    best_ids=np.where(results==np.max(results))
+    #np.save("hyp_tunin.npy", results)
+    best_ids=np.where(results==np.min(results))
 
     return hidden_layers_array[best_ids[0][0]],neurons_array[best_ids[1][0]],lr_array[best_ids[2][0]],n_array[best_ids[3][0]]
 
@@ -371,19 +356,21 @@ def example_main():
     x_test = test.loc[:, data.columns != output_label]
     y_test = test.loc[:, [output_label]]
 
-    regressor = Regressor(x_train, hidden_layers=5,neurons=30,learning_rate=0.1, nb_epoch = 1000,dropout=0.25)
-    regressor.fit(x_train, y_train,x_val=x_val,y_val=y_val,mini_batch_size=100,early_stop_n=5)
-    
+    regressor = Regressor(x_train, hidden_layers=1, neurons=30, learning_rate=0.1, nb_epoch=100, dropout=0.5)
+    regressor.fit(x_train, y_train, x_val=x_val, y_val=y_val, mini_batch_size=50, early_stop_n=5)
+
     # Error
     error = regressor.score(x_test, y_test)
     print("\nRegressor error: {}\n".format(error))
+    save_regressor(regressor)
+    
     print("\nPerforming Hyper Parameter Tuning")
-    best_hidden_layers,best_neurons,best_learning_rate,best_early_stop_n=RegressorHyperParameterSearch(x_train,y_train,x_val=x_val,y_val=y_val,x_test=x_test,y_test=y_test)
+    best_hidden_layers, best_neurons, best_learning_rate, best_early_stop_n = RegressorHyperParameterSearch(x_train,y_train,x_val,y_val)
     print('\nBest Params:')
     print('Hidden Layers: ',best_hidden_layers)
     print('Neurons: ',best_neurons)
     print('Learning Rate', best_learning_rate)
-    print('Best Early Stop n', best_learning_rate)
+    print('Best Early Stop n', best_early_stop_n)
 
     print("\nTraining With Best Params")
     best_regressor=Regressor(x_train,hidden_layers=best_hidden_layers,neurons=best_neurons,learning_rate=best_learning_rate,nb_epoch=1000,dropout=0.25)
@@ -392,7 +379,7 @@ def example_main():
 
     print("\nBest Regressor error: {}\n".format(best_error))
     save_regressor(best_regressor)
-    
+
 if __name__ == "__main__":
     example_main()
 
